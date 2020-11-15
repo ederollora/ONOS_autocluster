408fd8593c4c8747062df27a1a48313308fb9cb7
Michael Müller <michael.mueller2@haw-hamburg.de>
2020-11-15 14:51:48 +0100

    remove quotes to insert ips as seperate arguments

--- create_cluster.sh
+++ create_cluster.sh
@@ -246,7 +246,7 @@ apply_atomix_config(){
   do
     pos=$((i-1))
     cd
-    ./onos/tools/test/bin/atomix-gen-config "${allocatedAtomixIps[$pos]}" /tmp/atomix-$i.conf "${allocatedAtomixIps[*]}" >/dev/null
+    ./onos/tools/test/bin/atomix-gen-config "${allocatedAtomixIps[$pos]}" /tmp/atomix-$i.conf ${allocatedAtomixIps[*]} >/dev/null
     sudo docker cp /tmp/atomix-$i.conf atomix-$i:/opt/atomix/conf/atomix.conf
     sudo docker container start atomix-$i >/dev/null
     echo "Starting container atomix-$i"
